7467f0f312c83f155d60b8c9b78f03a1729d0c06
prahvin <prahvin@hotmail.com>
2022-11-10 11:31:57 +0530

    Added documentation

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # Introduction 
-TODO: Give a short introduction of your project. Let this section explain the objectives or the motivation behind this project. 
+Scenario based testing enabler using Gatling.
 
 # Getting Started
-TODO: Guide users through getting your code up and running on their own system. In this section you can talk about:
 1.	Installation process
+    Install gatling by referring https://gatling.io/docs/gatling/tutorials/installation/ 
 2.	Software dependencies
-3.	Latest releases
-4.	API references
+    Java Version 8,11 or 17.
 
 # Build and Test
-TODO: Describe and show how to build your code and run the tests. 
+On windows, Open "Command Prompt" window. 
+Change to ..\PerfTest\bin directory.
+Run gatling_scorpio_subscription.bat
+1- Press 1 to choose "[1] Run the Simulation locally" option.
+2- In "Choose a simulation number" to run the scenario contained in the file.
+3- Enter description for the test
 
-# Contribute
-TODO: Explain how other users and developers can contribute to make your code better. 
-
-If you want to learn more about creating good readme files then refer the following [guidelines](https://docs.microsoft.com/en-us/azure/devops/repos/git/create-a-readme?view=azure-devops). You can also seek inspiration from the below readme files:
-- [ASP.NET Core](https://github.com/aspnet/Home)
-- [Visual Studio Code](https://github.com/Microsoft/vscode)
-- [Chakra Core](https://github.com/Microsoft/ChakraCore)
+# Contact Us
+if you have questions about PerfTest, or you would like to reach out to us about an issue you're having , you can reach us as at <PVE@kmd.dk TODO: update with technical support email-id>
--- user-files/simulations/PublisherSimulationWebhook.java
+++ user-files/simulations/PublisherSimulationWebhook.java
@@ -6,11 +6,15 @@ import io.gatling.javaapi.http.*;
 
 import java.io.IOException;
 
+/**
+ * Builds a test scenario for check subscription feature of FIWARE.
+ */
 public class PublisherSimulationWebhook extends Simulation {
         public static void main(String[] args) throws IOException {
                 System.out.println("I just got executed!");
         }
 
+        //Obtain configuration values.
         public String baseUrl = System.getProperty("baseUrl");
         public String keyCloakUrl = System.getProperty("keyCloakUrl");
         public String args_client_id = System.getProperty("client_id");
@@ -18,6 +22,7 @@ public class PublisherSimulationWebhook extends Simulation {
         public String args_password = System.getProperty("password");
         public String args_grant_type = System.getProperty("grant_type");
 
+        //Creating common header for http request
         HttpProtocolBuilder httpProtocol = http
                         .baseUrl(baseUrl)
                         .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
@@ -27,90 +32,107 @@ public class PublisherSimulationWebhook extends Simulation {
                         .userAgentHeader(
                                         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0");
 
-    ScenarioBuilder scn = scenario("subscriptions")
-            .exec(http("Request Access Token")
-                    .post(keyCloakUrl)
-                    .header("Content-Type", "application/x-www-form-urlencoded")
-                    .formParam("client_id", args_client_id)
-                    .formParam("username", args_username)
-                    .formParam("password", args_password)
-                    .formParam("grant_type", args_grant_type)
-                    .check(status().is(200))
-                    .check(jsonPath("$.token_type").find().saveAs("token_type"))
-                    .check(jsonPath("$.access_token").find().saveAs("access_token"))
-                    .check(jsonPath("$.refresh_token").find().saveAs("refresh_token")))
-            .pause(1)
+        //Creating scenario for testing subscription                                
+        ScenarioBuilder scn = scenario("subscriptions")
+                        .exec(http("Request Access Token")
+                                        .post(keyCloakUrl)
+                                        .header("Content-Type", "application/x-www-form-urlencoded")
+                                        .formParam("client_id", args_client_id)
+                                        .formParam("username", args_username)
+                                        .formParam("password", args_password)
+                                        .formParam("grant_type", args_grant_type)
+                                        .check(status().is(200))
+                                        .check(jsonPath("$.token_type").find().saveAs("token_type"))
+                                        .check(jsonPath("$.access_token").find().saveAs("access_token"))
+                                        .check(jsonPath("$.refresh_token").find().saveAs("refresh_token")))
+                        .pause(1)
 
-            .exec(http("GET - Get Types")
-                    .get("/types")
-                    .header("content-type", "application/ld+json")
-                    .header("Authorization", session -> {
-                        return ((String) session.get("token_type")) + " " + ((String) session.get("access_token"));
-                    }))
-            .exec( http("GET - Get webhook token")
-                    .post("https://webhook.site/token")
-                    .check(jsonPath("$.uuid").find().saveAs("webhook_uuid")))
-                    .pause(1)
+                        .exec(http("GET - Get Types")
+                                        .get("/types")
+                                        .header("content-type", "application/ld+json")
+                                        .header("Authorization", session -> {
+                                                return ((String) session.get("token_type")) + " "
+                                                                + ((String) session.get("access_token"));
+                                        }))
+                        .exec(http("GET - Get webhook token")
+                                        .post("https://webhook.site/token")
+                                        .check(jsonPath("$.uuid").find().saveAs("webhook_uuid")))
+                        .pause(1)
 
-            .exec(http("POST - Create Subscription")
-                    .post("/subscriptions")
-                    .header("content-type", "application/ld+json")
-                    .header("Authorization", session -> {
-                        return ((String) session.get("token_type")) + " " + ((String) session.get("access_token"));
-                    })
-                    //.body(RawFileBody("new-subscription-data.json")))
-                    .body(StringBody(   session -> {
-                                        return "\r\n  {\r\n  \"id\": \"urn:subscription:1088\",\r\n  \"type\": \"Subscription\",\r\n  \"entities\": [{\r\n                \"type\": \"https:\\/\\/uri.fiware.org\\/ns\\/data-models#Building\"\r\n  }],\r\n   \"watchedAttributes\": [\"temperature\"],\r\n  \"notification\": {\r\n          \"attributes\": [\"temperature\"],\r\n    \"format\": \"normalized\",\r\n        \"endpoint\": {\r\n                \"uri\": \"https:\\/\\/webhook.site\\/"+((String) session.get("webhook_uuid"))+"\",\r\n                \"accept\": \"application\\/json\"\r\n        }\r\n  },\r\n    \"@context\": [\r\n    \"https:\\/\\/smartdatamodels.org\\/context.jsonld\",\r\n    \"https:\\/\\/uri.etsi.org\\/ngsi-ld\\/v1\\/ngsi-ld-core-context.jsonld\"\r\n  ]\r\n}"; })))
-            .pause(1)
-            .exec(
-                    http("POST - Create New Entity")
-                            .post("/entities")
-                            .header("content-type", "application/ld+json")
-                            .header("Authorization", session -> {
-                                return ((String) session.get("token_type")) + " "
-                                        + ((String) session.get("access_token"));
-                            })
-                            .body(RawFileBody("new-entity-data.json")))
-            .pause(1)
-            .exec(
-                    http("POST - Upsert Entity")
-                            .post("/entityOperations/upsert")
-                            .header("content-type", "application/ld+json")
-                            .header("Authorization", session -> {
-                                return ((String) session.get("token_type")) + " "
-                                        + ((String) session.get("access_token"));
-                            })
-                            .body(RawFileBody("update-entity-data.json")))
-            .pause(1)
-            .exec(
-                    http("GET - Webhook data")
-                            .get(session -> {System.out.println("Webhook URL: "+ "https://webhook.site/token/"+((String) session.get("webhook_uuid"))+"/request/latest" );
-                            return "https://webhook.site/token/"+((String) session.get("webhook_uuid"))+"/request/latest";})
-                            .header("content-type", "application/json")
-                            .check(status().is(200))
-                            // .check(jsonPath("$.subscriptionId").find().is("urn:subscription:1088"))
-                            .check(substring("urn:subscription:1088"))
+                        .exec(http("POST - Create Subscription")
+                                        .post("/subscriptions")
+                                        .header("content-type", "application/ld+json")
+                                        .header("Authorization", session -> {
+                                                return ((String) session.get("token_type")) + " "
+                                                                + ((String) session.get("access_token"));
+                                        })
+                                        // .body(RawFileBody("new-subscription-data.json")))
+                                        .body(StringBody(session -> {
+                                                return "\r\n  {\r\n  \"id\": \"urn:subscription:1088\",\r\n  \"type\": \"Subscription\",\r\n  \"entities\": [{\r\n                \"type\": \"https:\\/\\/uri.fiware.org\\/ns\\/data-models#Building\"\r\n  }],\r\n   \"watchedAttributes\": [\"temperature\"],\r\n  \"notification\": {\r\n          \"attributes\": [\"temperature\"],\r\n    \"format\": \"normalized\",\r\n        \"endpoint\": {\r\n                \"uri\": \"https:\\/\\/webhook.site\\/"
+                                                                + ((String) session.get("webhook_uuid"))
+                                                                + "\",\r\n                \"accept\": \"application\\/json\"\r\n        }\r\n  },\r\n    \"@context\": [\r\n    \"https:\\/\\/smartdatamodels.org\\/context.jsonld\",\r\n    \"https:\\/\\/uri.etsi.org\\/ngsi-ld\\/v1\\/ngsi-ld-core-context.jsonld\"\r\n  ]\r\n}";
+                                        })))
+                        .pause(1)
+                        .exec(
+                                        http("POST - Create New Entity")
+                                                        .post("/entities")
+                                                        .header("content-type", "application/ld+json")
+                                                        .header("Authorization", session -> {
+                                                                return ((String) session.get("token_type")) + " "
+                                                                                + ((String) session
+                                                                                                .get("access_token"));
+                                                        })
+                                                        .body(RawFileBody("new-entity-data.json")))
+                        .pause(1)
+                        .exec(
+                                        http("POST - Upsert Entity")
+                                                        .post("/entityOperations/upsert")
+                                                        .header("content-type", "application/ld+json")
+                                                        .header("Authorization", session -> {
+                                                                return ((String) session.get("token_type")) + " "
+                                                                                + ((String) session
+                                                                                                .get("access_token"));
+                                                        })
+                                                        .body(RawFileBody("update-entity-data.json")))
+                        .pause(1)
+                        .exec(
+                                        http("GET - Webhook data")
+                                                        .get(session -> {
+                                                                System.out.println("Webhook URL: "
+                                                                                + "https://webhook.site/token/"
+                                                                                + ((String) session.get("webhook_uuid"))
+                                                                                + "/request/latest");
+                                                                return "https://webhook.site/token/"
+                                                                                + ((String) session.get("webhook_uuid"))
+                                                                                + "/request/latest";
+                                                        })
+                                                        .header("content-type", "application/json")
+                                                        .check(status().is(200))
+                                                        // .check(jsonPath("$.subscriptionId").find().is("urn:subscription:1088"))
+                                                        .check(substring("urn:subscription:1088"))
 
-            )
-            .pause(1)
-            .exec(
-                    http("DELETE - Entity")
-                            .delete("/entities/urn:ngsi-ld:Gatling-Test-Building")
-                            .header("Authorization", session -> {
-                                return ((String) session.get("token_type")) + " "
-                                        + ((String) session.get("access_token"));
-                            }))
-            .pause(1)
-            .exec(
-                    http("DELETE - Subscription")
-                            .delete("/subscriptions/urn:subscription:1088")
-                            .header("Authorization", session -> {
-                                return ((String) session.get("token_type")) + " "
-                                        + ((String) session.get("access_token"));
-                            }));
-    {
-        setUp(
-                scn.injectOpen(rampUsers(1).during(10)))
-                .protocols(httpProtocol);
-    }
+                        )
+                        .pause(1)
+                        .exec(
+                                        http("DELETE - Entity")
+                                                        .delete("/entities/urn:ngsi-ld:Gatling-Test-Building")
+                                                        .header("Authorization", session -> {
+                                                                return ((String) session.get("token_type")) + " "
+                                                                                + ((String) session
+                                                                                                .get("access_token"));
+                                                        }))
+                        .pause(1)
+                        .exec(
+                                        http("DELETE - Subscription")
+                                                        .delete("/subscriptions/urn:subscription:1088")
+                                                        .header("Authorization", session -> {
+                                                                return ((String) session.get("token_type")) + " "
+                                                                                + ((String) session
+                                                                                                .get("access_token"));
+                                                        }));
+        {
+                setUp(
+                                scn.injectOpen(rampUsers(1).during(10)))
+                                .protocols(httpProtocol);
+        }
 }
